7d1c968ff0576991d5c1af6abbe5859450864c22
Dylan Lin <dylanmail0203.gmail>
2015-12-29 12:09:16 +0800

    add gem omniauth-facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,6 +239,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "738775506253771", "ce56fe8e966f64ab35182eeb39c7bd05",
+                   scope: 'public_profile,email', info_fields: 'email,name'
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
